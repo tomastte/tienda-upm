ee4327601f6068aa3e404a726a58b5a1030fcaa4
martssmt <lozanomarcosmarta@gmail.com>
2025-10-01 09:47:16 +0200

    feat(controller): CommandController-> Constructor and parseCommand() implemented

--- src/main/java/es/upm/etsisi/poo/Controller/CommandController.java
+++ src/main/java/es/upm/etsisi/poo/Controller/CommandController.java
@@ -1,4 +1,47 @@
 package es.upm.etsisi.poo.Controller;
 
+import es.upm.etsisi.poo.View.ConsoleView;
+
+import java.util.Arrays;
+
 public class CommandController {
+
+    private final ProductController productController;
+    private final TicketController ticketController;
+    private final ConsoleView view;
+
+    public CommandController(ProductController productController, TicketController ticketController, ConsoleView view) {
+        this.productController = productController;
+        this.ticketController = ticketController;
+        this.view = view;
+    }
+
+    public void parseCommand(String input) {
+
+        String[] tokens = input.trim().split(" ");
+        if (tokens.length == 0) return;
+
+        switch (tokens[0].toLowerCase()) {
+            case "prod":
+                handleProduct(tokens);
+                break;
+            case "ticket":
+                handleTicket(tokens);
+                break;
+            case "help":
+                view.showHelp();
+                break;
+            case "echo":
+                handleEcho(tokens);
+                break;
+            case "exit":
+                System.exit(0);
+                break;
+            default:
+                view.showMessage("Unknown command: " + input);
+                break;
+        }
+
+    }
+
 }
