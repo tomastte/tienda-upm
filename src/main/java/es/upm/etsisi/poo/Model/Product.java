c80f58dcf11ad03a92ff920234a51388acf14891
tomastte <juarezgelardoto@gmail.com>
2025-10-01 12:03:11 +0200

    feat(model): implement Product class

--- src/main/java/es/upm/etsisi/poo/Model/Product.java
+++ src/main/java/es/upm/etsisi/poo/Model/Product.java
@@ -1,4 +1,56 @@
 package es.upm.etsisi.poo.Model;
 
 public class Product {
+    private int id;
+    private String name;
+    private Category category;
+    private double price;
+
+    public Product(int id, String name, Category category, double price) {
+        this.id=id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public void setId(int id){
+        this.id=id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void setPrice(double price) {
+       this.price=price;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Category getCategory() {
+        return this.category;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + this.name + '\'' +
+                ", category=" + this.category +
+                ", price=" + this.price +
+                '}';
+    }
 }
