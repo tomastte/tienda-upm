2e01f06aa66c1509ef10419bb104ad8f91276ef3
martssmt <lozanomarcosmarta@gmail.com>
2025-10-02 00:37:29 +0200

    refactor: CommandController-> Whole class refactored with helper methods

--- src/main/java/es/upm/etsisi/poo/Controller/CommandController.java
+++ src/main/java/es/upm/etsisi/poo/Controller/CommandController.java
@@ -24,29 +24,21 @@ public class CommandController {
         String[] tokens = input.trim().split(" ");
 
         switch (tokens[0].toLowerCase()) {
-            case "prod":
-                this.handleProduct(tokens, input);
-                break;
-            case "ticket":
-                this.handleTicket(tokens, input);
-                break;
-            case "help":
-                this.view.showHelp();
-                break;
-            case "echo":
-                this.handleEcho(tokens);
-                break;
-            case "exit":
+            case "prod" -> this.handleProduct(tokens, input);
+            case "ticket" -> this.handleTicket(tokens, input);
+            case "help" -> this.view.showHelp();
+            case "echo" -> this.handleEcho(tokens);
+            case "exit" -> {
                 this.view.showMessage("Closing application.");
                 this.view.showMessage("Goodbye!");
                 System.exit(0);
-                break;
-            default:
-                this.view.showMessage("Unknown command: " + input);
-                break;
+            }
+            default -> this.view.showMessage("Unknown command: " + input);
         }
 
     }
+
+    // Product:
 
     private void handleProduct(String[] tokens, String input) {
 
@@ -56,75 +48,83 @@ public class CommandController {
         }
 
         switch (tokens[1].toLowerCase()) {
-            case "add":
-                try {
-                    int firstQuote = input.indexOf('"');
-                    int lastQuote = input.lastIndexOf('"');
-                    if (firstQuote == -1 || firstQuote == lastQuote) {
-                        throw new IllegalArgumentException("Name must be in quotes");
-                    }
-
-                    String name = input.substring(firstQuote + 1, lastQuote);
-                    String[] beforeName = input.substring(0, firstQuote).split(" ");
-                    String[] afterName = input.substring(lastQuote + 1).trim().split(" ");
-                    if (afterName.length != 2 || beforeName.length != 3) {
-                        throw new IllegalArgumentException("Usage: prod add <id> \\\"<name>\\\" <category> <price>");
-                    }
-
-                    int id = Integer.parseInt(beforeName[2]);
-                    Category category = Category.valueOf(afterName[0].toUpperCase());
-                    double price = Double.parseDouble(afterName[1]);
-
-                    this.productController.handleAdd(id, name, category, price);
-                } catch (Exception e) {
-                    this.view.showMessage("Error in prod add: " + e.getMessage());
-                }
-                break;
-            case "list":
-                try {
-                    if (tokens.length != 2) {
-                        throw new IllegalArgumentException("Usage: prod list");
-                    }
-                    this.productController.handleList();
-                } catch (Exception e) {
-                    this.view.showMessage("Error in prod list: " + e.getMessage());
-                }
-                break;
-            case "update":
-                try {
-                    if (tokens.length < 5) {
-                        throw new IllegalArgumentException("Usage: prod update <id> NAME|CATEGORY|PRICE <value>");
-                    }
-                    int idUpdate = Integer.parseInt(tokens[2]);
-                    String field = tokens[3];
-                    if (!field.equals("NAME") && !field.equals("CATEGORY") && !field.equals("PRICE")) {
-                        throw new IllegalArgumentException("Field must be NAME, CATEGORY or PRICE");
-                    }
-                    String value = String.join(" ", Arrays.copyOfRange(tokens, 4, tokens.length));
-                    this.productController.handleUpdate(idUpdate, field, value);
-                } catch (Exception e) {
-                    view.showMessage("Error in prod update: " + e.getMessage());
-                }
-                break;
-            case "remove":
-                try {
-                    if (tokens.length != 3) {
-                        throw new IllegalArgumentException("Usage: prod remove <id>");
-                    }
-                    int idRemove = Integer.parseInt(tokens[2]);
-                    this.productController.handleRemove(idRemove);
-                } catch (Exception e) {
-                    this.view.showMessage("Error in prod remove: " + e.getMessage());
-                }
-                break;
-            case default:
-                this.view.showMessage("Unknown command: " + input);
-                break;
+            case "add" -> this.productAdd(tokens, input);
+            case "list" -> this.productList(tokens);
+            case "update" -> this.productUpdate(tokens);
+            case "remove" -> this.productRemove(tokens);
+            case default -> this.view.showMessage("Unknown command: " + input);
         }
 
     }
 
-    private void handleTicket(String[] tokens, String input) {
+    private void productAdd(String[] tokens, String input) {
+        try {
+            int firstQuote = input.indexOf('"');
+            int lastQuote = input.lastIndexOf('"');
+            if (firstQuote == -1 || firstQuote == lastQuote) {
+                throw new IllegalArgumentException("Name must be in quotes");
+            }
+
+            String name = input.substring(firstQuote + 1, lastQuote);
+            String[] beforeName = input.substring(0, firstQuote).split(" ");
+            String[] afterName = input.substring(lastQuote + 1).trim().split(" ");
+            if (afterName.length != 2 || beforeName.length != 3) {
+                throw new IllegalArgumentException("Usage: prod add <id> \\\"<name>\\\" <category> <price>");
+            }
+
+            int id = Integer.parseInt(beforeName[2]);
+            Category category = Category.valueOf(afterName[0].toUpperCase());
+            double price = Double.parseDouble(afterName[1]);
+
+            this.productController.handleAdd(id, name, category, price);
+        } catch (Exception e) {
+            this.view.showMessage("Error in prod add: " + e.getMessage());
+        }
+    }
+
+    private void productList(String[] tokens) {
+        try {
+            if (tokens.length != 2) {
+                throw new IllegalArgumentException("Usage: prod list");
+            }
+            this.productController.handleList();
+        } catch (Exception e) {
+            this.view.showMessage("Error in prod list: " + e.getMessage());
+        }
+    }
+
+    private void productUpdate(String[] tokens) {
+        try {
+            if (tokens.length < 5) {
+                throw new IllegalArgumentException("Usage: prod update <id> NAME|CATEGORY|PRICE <value>");
+            }
+            int idUpdate = Integer.parseInt(tokens[2]);
+            String field = tokens[3];
+            if (!field.equals("NAME") && !field.equals("CATEGORY") && !field.equals("PRICE")) {
+                throw new IllegalArgumentException("Field must be NAME, CATEGORY or PRICE");
+            }
+            String value = String.join(" ", Arrays.copyOfRange(tokens, 4, tokens.length));
+            this.productController.handleUpdate(idUpdate, field, value);
+        } catch (Exception e) {
+            this.view.showMessage("Error in prod update: " + e.getMessage());
+        }
+    }
+
+    private void productRemove(String[] tokens) {
+        try {
+            if (tokens.length != 3) {
+                throw new IllegalArgumentException("Usage: prod remove <id>");
+            }
+            int idRemove = Integer.parseInt(tokens[2]);
+            this.productController.handleRemove(idRemove);
+        } catch (Exception e) {
+            this.view.showMessage("Error in prod remove: " + e.getMessage());
+        }
+    }
+
+    // Ticket:
+
+    private void handleTicket(String[] tokens) {
 
         if (tokens.length < 2) {
             this.view.showMessage("Error: ticket command needs arguments");
@@ -132,48 +132,53 @@ public class CommandController {
         }
 
         switch (tokens[1].toLowerCase()) {
-            case "new":
-                try {
-                    if (tokens.length != 2) {
-                        throw new IllegalArgumentException("Usage: ticket new");
-                    }
-                    this.ticketController.handleNew();
-                } catch (Exception e) {
-                    this.view.showMessage("Error in ticket new: " + e.getMessage());
-                }
-                break;
-            case "add":
-                try {
-                    if (tokens.length != 4) {
-                        throw new IllegalArgumentException("Usage: ticket add <prodId> <quantity>");
-                    }
-                    int idAdd = Integer.parseInt(tokens[2]);
-                    int quantity = Integer.parseInt(tokens[3]);
-                    this.ticketController.handleAdd(idAdd, quantity);
-                } catch (Exception e) {
-                    view.showMessage("Error in ticket add: " + e.getMessage());
-                }
-                break;
-            case "remove":
-                try {
-                    if (tokens.length < 3) {
-                        throw new IllegalArgumentException("Usage: ticket remove <prodId>");
-                    }
-                    int idRemove = Integer.parseInt(tokens[2]);
-                    this.ticketController.handleRemove(idRemove);
-                } catch (Exception e) {
-                    view.showMessage("Error in ticket remove: " + e.getMessage());
-                }
-                break;
-            case "print":
-                this.ticketController.handlePrint();
-                break;
-            case default:
-                this.view.showMessage("Unknown command: " + String.join(" ", Arrays.copyOfRange(tokens, 0, tokens.length)));
-                break;
+            case "new" -> this.ticketNew(tokens);
+            case "add" -> this.ticketAdd(tokens);
+            case "remove" -> this.ticketRemove(tokens);
+            case "print" -> this.ticketController.handlePrint();
+            case default ->
+                    this.view.showMessage("Unknown command: " + String.join(" ", Arrays.copyOfRange(tokens, 0, tokens.length)));
         }
 
     }
+
+    private void ticketNew(String[] tokens) {
+        try {
+            if (tokens.length != 2) {
+                throw new IllegalArgumentException("Usage: ticket new");
+            }
+            this.ticketController.handleNew();
+        } catch (Exception e) {
+            this.view.showMessage("Error in ticket new: " + e.getMessage());
+        }
+    }
+
+    private void ticketAdd(String[] tokens) {
+        try {
+            if (tokens.length != 4) {
+                throw new IllegalArgumentException("Usage: ticket add <prodId> <quantity>");
+            }
+            int idAdd = Integer.parseInt(tokens[2]);
+            int quantity = Integer.parseInt(tokens[3]);
+            this.ticketController.handleAdd(idAdd, quantity);
+        } catch (Exception e) {
+            this.view.showMessage("Error in ticket add: " + e.getMessage());
+        }
+    }
+
+    private void ticketRemove(String[] tokens) {
+        try {
+            if (tokens.length < 3) {
+                throw new IllegalArgumentException("Usage: ticket remove <prodId>");
+            }
+            int idRemove = Integer.parseInt(tokens[2]);
+            this.ticketController.handleRemove(idRemove);
+        } catch (Exception e) {
+            this.view.showMessage("Error in ticket remove: " + e.getMessage());
+        }
+    }
+
+    // Echo
 
     private void handleEcho(String[] tokens) {
         this.view.showMessage(String.join(" ", Arrays.copyOfRange(tokens, 1, tokens.length)));
