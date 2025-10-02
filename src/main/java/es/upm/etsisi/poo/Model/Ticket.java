b46fdb927c727e73550ddef343eb38afd9d03d52
jl721 <jilingli1227@gmail.com>
2025-10-02 22:03:50 +0200

    feat(model): Ticket -> Constructor and method addProduct(Product product, int quantity) implemented

--- src/main/java/es/upm/etsisi/poo/Model/Ticket.java
+++ src/main/java/es/upm/etsisi/poo/Model/Ticket.java
@@ -1,5 +1,34 @@
 package es.upm.etsisi.poo.Model;
 
+import java.util.*;
+
 public class Ticket {
+    private final LinkedList<TicketItem> itemList;
+
+    public Ticket(){
+        this.itemList = new LinkedList<>();
+    }
+
+    public boolean addProduct(Product product, int quantity) {
+        boolean result = true;
+        if(product == null || quantity <= 0){
+            result = false;
+        }
+        boolean itemFound = false;
+        Iterator<TicketItem> iterator = this.itemList.iterator();
+        while(iterator.hasNext() && !itemFound){
+            TicketItem item = iterator.next();
+            if(item.getProduct().equals(product)){
+                itemFound = true;
+                item.setQuantity(item.getQuantity() + quantity);
+            }
+        }
+        if(!itemFound){
+            TicketItem newItem = new TicketItem(product, quantity, product.getCategory().getDiscount());
+            itemList.add(newItem);
+        }
+
+        return result;
+    }
 
 }
