145baff71a0023367a7b4112ff761226e73c8198
sofiam32 <sofiamerfer@gmail.com>
2025-10-03 14:31:39 +0200

    feat(view): method main implemented in App class

--- src/main/java/es/upm/etsisi/poo/App.java
+++ src/main/java/es/upm/etsisi/poo/App.java
@@ -1,9 +1,35 @@
 package es.upm.etsisi.poo;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import es.upm.etsisi.poo.Controller.CommandController;
+import es.upm.etsisi.poo.Controller.ProductController;
+import es.upm.etsisi.poo.Controller.TicketController;
+import es.upm.etsisi.poo.Model.Catalog;
+import es.upm.etsisi.poo.View.ConsoleView;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class App {
+    public static void main(String[] args) {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            ConsoleView view = new ConsoleView();
+            ProductController productController = new ProductController(view);
+            Catalog catalog = productController.getCatalog();
+            TicketController ticketController = new TicketController(view, catalog);
+            CommandController comand = new CommandController(productController, ticketController, view);
+            String line = "";
+            while (!line.equals("exit")) {
+                System.out.print("tUPM> ");
+                line = reader.readLine();
+                comand.parseCommand(line);
+            }
+            System.out.println("Closing application.");
+            System.out.println("Goodbye!");
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
     }
 }
