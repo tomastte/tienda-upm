6e55f374a6b02a1df9dfba7b425ae5d687799296
tomastte <juarezgelardoto@gmail.com>
2025-10-06 19:28:10 +0200

    fix(model) rename constants

--- src/main/java/es/upm/etsisi/poo/Model/Category.java
+++ src/main/java/es/upm/etsisi/poo/Model/Category.java
@@ -2,10 +2,10 @@ package es.upm.etsisi.poo.Model;
 
 public enum Category {
     MERCH(0),
-    PAPELERIA(0.05),
-    ROPA(0.07),
-    LIBRO(0.1),
-    ELECTRONICA(0.03);
+    STATIONERY(0.05),
+    CLOTHES(0.07),
+    BOOK(0.1),
+    ELECTRONICS(0.03);
 
     private final double discount;
 
