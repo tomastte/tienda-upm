86865ed36245a0e9c97739bab3c72d60f719bbba
martssmt <lozanomarcosmarta@gmail.com>
2025-11-15 18:51:50 +0100

    feat(services): interface Service implemented

--- src/main/java/es/upm/etsisi/poo/app2/services/Service.java
+++ src/main/java/es/upm/etsisi/poo/app2/services/Service.java
@@ -1,4 +1,13 @@
 package es.upm.etsisi.poo.app2.services;
 
-public interface Service {
+import es.upm.etsisi.poo.app2.data.model.Entity;
+
+public interface Service<T extends Entity> {
+
+    public void add(T entity, String id);
+
+    public void remove(String id);
+
+    public void list();
+
 }
