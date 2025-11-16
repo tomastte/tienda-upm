abe0208e8b045d7e7834acad3a9d1c76e66076bd
Alicia Lafuente <alicia.lafuente.sanz@alumnos.upm.es>
2025-11-16 20:14:44 +0100

    feat(repositories): abstract methods added to the ClientRepository interface

--- src/main/java/es/upm/etsisi/poo/app2/data/repositories/ClientRepository.java
+++ src/main/java/es/upm/etsisi/poo/app2/data/repositories/ClientRepository.java
@@ -1,4 +1,9 @@
 package es.upm.etsisi.poo.app2.data.repositories;
 
+import es.upm.etsisi.poo.app2.data.model.user.Client;
+
 public interface ClientRepository extends Repository {
+
+    Client findByMail(String mail);
+
 }
