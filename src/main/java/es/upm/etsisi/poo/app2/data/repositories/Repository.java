331d04a730a613c8735ea11433bc7a2a7d443d96
Alicia Lafuente <alicia.lafuente.sanz@alumnos.upm.es>
2025-11-16 20:04:11 +0100

    feat(repositories): abstract methods added to the Repository interface

--- src/main/java/es/upm/etsisi/poo/app2/data/repositories/Repository.java
+++ src/main/java/es/upm/etsisi/poo/app2/data/repositories/Repository.java
@@ -1,4 +1,17 @@
 package es.upm.etsisi.poo.app2.data.repositories;
 
-public interface Repository {
+import es.upm.etsisi.poo.app2.data.model.Entity;
+
+import java.util.List;
+
+public interface Repository<T extends Entity> {
+
+    public void add(T entity, String id);
+
+    public void remove(String id);
+
+    public List<Entity>list();
+
+    public Entity findById(String id);
+
 }
