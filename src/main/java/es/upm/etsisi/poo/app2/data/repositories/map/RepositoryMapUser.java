b5ed0447b243a4ff8be3855350672a7fad60bdc6
Alicia Lafuente <alicia.lafuente.sanz@alumnos.upm.es>
2025-11-17 18:18:42 +0100

    feat(repositories): RepositoryMapUser structure

--- src/main/java/es/upm/etsisi/poo/app2/data/repositories/map/RepositoryMapUser.java
+++ src/main/java/es/upm/etsisi/poo/app2/data/repositories/map/RepositoryMapUser.java
@@ -1,8 +1,47 @@
-package es.upm.etsisi.poo.app2.data.repositories.map;
-
 import es.upm.etsisi.poo.app2.data.model.user.User;
 import es.upm.etsisi.poo.app2.data.repositories.RepositoryUser;
 
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
 public abstract class RepositoryMapUser<T extends User> implements RepositoryUser<T> {
 
+    protected final Map<String, T> map;
+    protected String id;
+
+    public RepositoryMapUser(){
+        this.map = new HashMap<>();
+        this.id = "";
+    }
+
+    @Override
+    public void add(T entity, String id) {
+
+    }
+
+    @Override
+    public void remove(String id) {
+
+    }
+
+    @Override
+    public List<T> list() {
+        return null;
+    }
+
+    @Override
+    public T findById(String id) {
+       return null;
+    }
+
+    @Override
+    public T findByMail(String mail) {
+      return null;
+    }
+
 }
+
