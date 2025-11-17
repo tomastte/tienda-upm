573000103b92c67505840766ee9f7b6c05cecd69
jl721 <jilingli1227@gmail.com>
2025-11-18 00:31:08 +0100

    feat(model): class BasicProduct implemented

--- src/main/java/es/upm/etsisi/poo/app2/data/model/shop/BasicProduct.java
+++ src/main/java/es/upm/etsisi/poo/app2/data/model/shop/BasicProduct.java
@@ -1,4 +1,29 @@
 package es.upm.etsisi.poo.app2.data.model.shop;
 
 public class BasicProduct extends Product {
+    private Category category;
+
+    public BasicProduct(Integer id, String name, Category category, Double price){
+        super(id, name, price);
+        this.category = category;
+    }
+
+    public BasicProduct(String name, Category category, Double price){
+        super(name, price);
+        this.category = category;
+    }
+
+    public Category getCategory() {
+        return this.category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "{class:Product, id:" + getId() + ", name:'" + getName() +
+                "', category:" + category + ", price:" + getPrice() + "}";
+    }
 }
