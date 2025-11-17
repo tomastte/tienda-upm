a9b23abd3f64f2fdbf54f5643d2e6abf014e4bc2
jl721 <jilingli1227@gmail.com>
2025-11-17 23:24:35 +0100

    feat(model): abstract class Entity implemented

--- src/main/java/es/upm/etsisi/poo/app2/data/model/Entity.java
+++ src/main/java/es/upm/etsisi/poo/app2/data/model/Entity.java
@@ -1,4 +1,45 @@
 package es.upm.etsisi.poo.app2.data.model;
 
-public class Entity {
+import java.util.Objects;
+
+public abstract class Entity <T> {
+    private T id;
+
+    protected Entity(){
+        this.id = null;
+    }
+
+    protected Entity(T id){
+        this.id = id;
+    }
+
+    public T getId() {
+        return id;
+    }
+
+    public void setId(T id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object entity){
+        if(this == entity){
+            return true;
+        }
+        if(entity == null || this.getClass() != entity.getClass()){
+            return false;
+        }
+        Entity<?> other = (Entity<?>) entity;
+        return Objects.equals(this.id, other.id);
+    }
+
+    @Override
+    public int hashCode(){
+       return Objects.hash(this.id);
+    }
+
+    @Override
+    public String toString(){
+        return "Entity{id=" + id + "}";
+    }
 }
