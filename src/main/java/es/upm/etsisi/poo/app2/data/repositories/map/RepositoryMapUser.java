f48696fdf74a85f096ae43df4ee476e3b8e51a60
Alicia Lafuente <alicia.lafuente.sanz@alumnos.upm.es>
2025-11-17 19:41:06 +0100

    feat(repositories): methods completed in RepositoryMapUser

--- src/main/java/es/upm/etsisi/poo/app2/data/repositories/map/RepositoryMapUser.java
+++ src/main/java/es/upm/etsisi/poo/app2/data/repositories/map/RepositoryMapUser.java
@@ -1,12 +1,10 @@
 package es.upm.etsisi.poo.app2.data.repositories.map;
 
+import es.upm.etsisi.poo.app2.data.model.shop.Product;
 import es.upm.etsisi.poo.app2.data.model.user.User;
 import es.upm.etsisi.poo.app2.data.repositories.RepositoryUser;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public abstract class RepositoryMapUser<T extends User> implements RepositoryUser<T> {
 
@@ -20,27 +18,40 @@ public abstract class RepositoryMapUser<T extends User> implements RepositoryUse
 
     @Override
     public void add(T entity, String id) {
-
+        this.map.put(id, entity);
     }
 
     @Override
     public void remove(String id) {
-
+        this.map.remove(id);
     }
 
     @Override
     public List<T> list() {
-        return null;
+        return new ArrayList<>(this.map.values());
     }
 
     @Override
     public T findById(String id) {
-       return null;
+       return this.map.get(id);
     }
 
     @Override
     public T findByMail(String mail) {
-      return null;
+
+        boolean found = false;
+        T entity = null;
+        Iterator<T> iterator = this.map.values().iterator();
+
+        while (iterator.hasNext() && !found) {
+            T actualEntity = iterator.next();
+            String actualMail = actualEntity.getMail();
+            if (mail.equals(actualMail)) {
+                found = true;
+                entity = actualEntity;
+            }
+        }
+        return entity;
     }
 
 }
