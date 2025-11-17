b2f6c44c3b99febd066608e62e2582af24a96d17
Alicia Lafuente <alicia.lafuente.sanz@alumnos.upm.es>
2025-11-17 18:22:21 +0100

    feat(repositories): ProductRepositoryMap structure

--- src/main/java/es/upm/etsisi/poo/app2/data/repositories/map/ProductRepositoryMap.java
+++ src/main/java/es/upm/etsisi/poo/app2/data/repositories/map/ProductRepositoryMap.java
@@ -5,4 +5,19 @@ import es.upm.etsisi.poo.app2.data.repositories.ProductRepository;
 
 public class ProductRepositoryMap extends RepositoryMapShop<Product> implements ProductRepository {
 
+    public ProductRepositoryMap() {
+        super();
+    }
+
+    @Override
+    public void add(Product product) {
+
+    }
+
+    @Override
+    public boolean find(Product product) {
+        return true;
+    }
+
 }
+
