83d9dd8023c85f5eb069a4a28de2d9e24a15f595
Alicia Lafuente <alicia.lafuente.sanz@alumnos.upm.es>
2025-11-17 18:20:47 +0100

    feat(repositories): RepositoryMapShop structure

--- src/main/java/es/upm/etsisi/poo/app2/data/repositories/map/RepositoryMapShop.java
+++ src/main/java/es/upm/etsisi/poo/app2/data/repositories/map/RepositoryMapShop.java
@@ -3,6 +3,40 @@ package es.upm.etsisi.poo.app2.data.repositories.map;
 import es.upm.etsisi.poo.app2.data.model.Entity;
 import es.upm.etsisi.poo.app2.data.repositories.RepositoryShop;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public abstract class RepositoryMapShop<T extends Entity> implements RepositoryShop<T> {
 
+    protected final Map<Integer, T> map;
+    protected Integer id;
+
+    public RepositoryMapShop(){
+        this.map = new HashMap<>();
+        this.id = 1;
+    }
+
+    @Override
+    public void add(T entity, Integer id) {
+
+    }
+
+    @Override
+    public void remove(Integer id) {
+
+    }
+
+    @Override
+    public List<T> list() {
+        return null;
+    }
+
+    @Override
+    public T findById(Integer id) {
+        return null;
+    }
+
 }
+
