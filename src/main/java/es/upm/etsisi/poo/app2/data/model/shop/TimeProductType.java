62ebcbfeeef0f063b651ae38cf6edbbde712c844
jl721 <jilingli1227@gmail.com>
2025-11-18 01:48:26 +0100

    feat(model): TimeProductType enum implemented

--- src/main/java/es/upm/etsisi/poo/app2/data/model/shop/TimeProductType.java
+++ src/main/java/es/upm/etsisi/poo/app2/data/model/shop/TimeProductType.java
@@ -1,4 +1,16 @@
 package es.upm.etsisi.poo.app2.data.model.shop;
 
 public enum TimeProductType {
+    FOOD(72),
+    MEETING(12);
+
+    private final Integer PLANNING_HOURS;
+
+    TimeProductType(Integer planningHours) {
+        this.PLANNING_HOURS = planningHours;
+    }
+
+    public Integer getPLANNING_HOURS() {
+        return this.PLANNING_HOURS;
+    }
 }
