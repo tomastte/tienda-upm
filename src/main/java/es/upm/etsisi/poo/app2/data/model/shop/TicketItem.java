90e9719823ba6ba515004d16f796e3582cb9ec06
jl721 <jilingli1227@gmail.com>
2025-11-18 14:24:28 +0100

    feat(model): class TicketItem implemented

--- src/main/java/es/upm/etsisi/poo/app2/data/model/shop/TicketItem.java
+++ src/main/java/es/upm/etsisi/poo/app2/data/model/shop/TicketItem.java
@@ -1,4 +1,53 @@
 package es.upm.etsisi.poo.app2.data.model.shop;
 
+import java.util.Objects;
+
 public class TicketItem {
+    private final Product product;
+    private Integer quantity;
+    private Double discountApplied;
+
+    public TicketItem(Product product, Integer quantity) {
+        this.product = product;
+        this.quantity = quantity;
+        this.discountApplied = 0.0;
+    }
+
+    public Product getProduct() {
+        return this.product;
+    }
+
+    public Integer getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getDiscountApplied() {
+        return this.discountApplied;
+    }
+
+    public void setDiscountApplied(Double discountApplied) {
+        this.discountApplied = Objects.requireNonNullElse(discountApplied, 0.0);
+    }
+
+    public Double getSubtotal(){
+        return this.product.getPrice() * this.quantity;
+    }
+
+    public Double getDiscount() {
+        return this.discountApplied * this.getSubtotal();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(this.product.toString());
+        if (discountApplied != null && discountApplied > 0.0) {
+            stringBuilder.append(" **discount -").append(this.product.getPrice() * this.discountApplied);
+        }
+        return stringBuilder.toString();
+    }
 }
