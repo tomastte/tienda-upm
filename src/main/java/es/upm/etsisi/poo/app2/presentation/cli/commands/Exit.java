970643ec5d727bdc74f94bdf89e93a00ba6aa8b5
sofiam32 <sofiamerfer@gmail.com>
2025-11-18 13:29:56 +0100

    feat(cli): Implement Exit class.

--- src/main/java/es/upm/etsisi/poo/app2/presentation/cli/commands/Exit.java
+++ src/main/java/es/upm/etsisi/poo/app2/presentation/cli/commands/Exit.java
@@ -2,5 +2,26 @@ package es.upm.etsisi.poo.app2.presentation.cli.commands;
 
 import es.upm.etsisi.poo.app2.presentation.cli.Command;
 
+import java.util.List;
+
 public class Exit implements Command {
+
+    @Override
+    public String name() {
+        return "exit";
+    }
+
+    @Override
+    public List<String> params() {
+        return List.of();
+    }
+
+    @Override
+    public String helpMessage() {
+        return "Closes the application.";
+    }
+
+    @Override
+    public void execute(List<String> params) {
+    }
 }
