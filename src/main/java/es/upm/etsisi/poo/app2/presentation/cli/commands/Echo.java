81fc4530c59c696b595eaacebf287ea7b854c53e
sofiam32 <sofiamerfer@gmail.com>
2025-11-18 13:30:53 +0100

    feat(cli): Implement Echo class.

--- src/main/java/es/upm/etsisi/poo/app2/presentation/cli/commands/Echo.java
+++ src/main/java/es/upm/etsisi/poo/app2/presentation/cli/commands/Echo.java
@@ -1,6 +1,34 @@
 package es.upm.etsisi.poo.app2.presentation.cli.commands;
 
 import es.upm.etsisi.poo.app2.presentation.cli.Command;
+import es.upm.etsisi.poo.app2.presentation.view.View;
+
+import java.util.List;
 
 public class Echo implements Command {
+
+    @Override
+    public String name() {
+        return "echo";
+    }
+
+    @Override
+    public List<String> params() {
+        return List.of("\"<text>\"");
+    }
+
+    @Override
+    public String helpMessage() {
+        return "Prints the provided text";
+    }
+
+    @Override
+    public void execute(List<String> params) {
+        if (params.isEmpty()) {
+            View.show("\"\"");
+        } else {
+            String text = String.join(" ", params);
+            View.show("\"" + text + "\"");
+        }
+    }
 }
