8c81e9c99b09ed13efcfc9fc2591d684fba997ed
sofiam32 <sofiamerfer@gmail.com>
2025-11-18 13:25:33 +0100

    feat(cli): Implement Command class.

--- src/main/java/es/upm/etsisi/poo/app2/presentation/cli/Command.java
+++ src/main/java/es/upm/etsisi/poo/app2/presentation/cli/Command.java
@@ -1,4 +1,21 @@
 package es.upm.etsisi.poo.app2.presentation.cli;
 
+import java.util.List;
+
 public interface Command {
+    public String name();
+
+    public List<String> params();
+
+    public String helpMessage();
+
+    public void execute(List<String> params);
+
+    default String help() {
+        StringBuilder result = new StringBuilder(this.name());
+        if (!this.params().isEmpty()) {
+            result.append(" ").append(String.join(" ", this.params()));
+        }
+        return result.toString();
+    }
 }
