f1e502d6e00eaed2b9cc5e0d76d78cbd6b2ed715
sofiam32 <sofiamerfer@gmail.com>
2025-11-19 17:50:02 +0100

    feat(cli):Implement TicketList class.

--- src/main/java/es/upm/etsisi/poo/app2/presentation/cli/commands/ticket/TicketList.java
+++ src/main/java/es/upm/etsisi/poo/app2/presentation/cli/commands/ticket/TicketList.java
@@ -1,6 +1,39 @@
 package es.upm.etsisi.poo.app2.presentation.cli.commands.ticket;
 
+import es.upm.etsisi.poo.app2.data.model.shop.Ticket;
 import es.upm.etsisi.poo.app2.presentation.cli.Command;
+import es.upm.etsisi.poo.app2.presentation.view.View;
+import es.upm.etsisi.poo.app2.services.user.CashierService;
+
+import java.util.List;
 
 public class TicketList implements Command {
+
+    private final CashierService cashierService;
+
+    public TicketList(CashierService cashierService) {
+        this.cashierService = cashierService;
+    }
+
+    @Override
+    public String name() {
+        return "ticket list";
+    }
+
+    @Override
+    public List<String> params() {
+        return List.of();
+    }
+
+    @Override
+    public String helpMessage() {
+        return "Lists all tickets registered in the system.";
+    }
+
+    @Override
+    public void execute(List<String> params) {
+        List<Ticket> tickets = cashierService.ticketList();
+        View.showList("Ticket List:", tickets);
+        View.show("ticket list: ok");
+    }
 }
