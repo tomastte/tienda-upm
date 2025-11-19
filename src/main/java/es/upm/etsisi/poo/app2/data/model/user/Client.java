dad15eba7cd08b8ab92ea141afaecc2a9c4aec88
jl721 <jilingli1227@gmail.com>
2025-11-19 02:38:24 +0100

    feat(model): class Client implemented

--- src/main/java/es/upm/etsisi/poo/app2/data/model/user/Client.java
+++ src/main/java/es/upm/etsisi/poo/app2/data/model/user/Client.java
@@ -1,4 +1,23 @@
 package es.upm.etsisi.poo.app2.data.model.user;
 
 public class Client extends User{
+    private String cashierId;
+
+    public Client(String name, String mail, String cashierId){
+        super(cashierId, name, mail);
+    }
+
+    public String getCashierId() {
+        return this.cashierId;
+    }
+
+    public void setCashierId(String cashierId) {
+        this.cashierId = cashierId;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{identifier='" + getId() + "', name='" + getName() +
+                "', email='" + getMail() + "', cash=" + cashierId + "}";
+    }
 }
