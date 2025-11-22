bc280dcec6254d95e3b6e0a073c98dd34878bbe9
tomastte <juarezgelardoto@gmail.com>
2025-11-22 22:23:15 +0100

    ErrorHandler class implemented

--- src/main/java/es/upm/etsisi/poo/app2/presentation/cli/ErrorHandler.java
+++ src/main/java/es/upm/etsisi/poo/app2/presentation/cli/ErrorHandler.java
@@ -1,4 +1,31 @@
 package es.upm.etsisi.poo.app2.presentation.cli;
 
+import es.upm.etsisi.poo.app2.presentation.view.View;
+
+import java.io.IOException;
+
 public class ErrorHandler {
+    public void handlesErrors(CommandLineInterface commandLineInterface,
+                              View view, String[] args) {
+
+        view.showTitle("App Tienda UPM");
+        boolean exit = false;
+        while (!exit) {
+            try {
+
+                if (args.length == 0) {
+                    exit = commandLineInterface.runCommands();
+                } else {
+                    exit = commandLineInterface.runCommandsFromFile(args[0]);
+                }
+
+            } catch (IOException ioException){
+                exit=true;
+                view.showError("ERROR (" + ioException.getClass().getSimpleName() + ") >>> " + ioException.getMessage());
+            } catch (Exception e) {
+                view.showError("ERROR (" + e.getClass().getSimpleName() + ") >>> " + e.getMessage());
+            }
+        }
+        view.showTitle("Hasta pronto!");
+    }
 }
