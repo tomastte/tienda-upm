5dd0a6e9324c3ab3d3a0b4ecd92a33f80cb645e9
tomastte <juarezgelardoto@gmail.com>
2025-11-22 22:24:58 +0100

    CommandLineInterface class implemented

--- src/main/java/es/upm/etsisi/poo/app2/presentation/cli/CommandLineInterface.java
+++ src/main/java/es/upm/etsisi/poo/app2/presentation/cli/CommandLineInterface.java
@@ -1,4 +1,97 @@
 package es.upm.etsisi.poo.app2.presentation.cli;
 
+import es.upm.etsisi.poo.app2.presentation.cli.exceptions.BadRequestException;
+import es.upm.etsisi.poo.app2.presentation.cli.exceptions.CommandException;
+import es.upm.etsisi.poo.app2.presentation.view.View;
+
+import static es.upm.etsisi.poo.app2.presentation.cli.Command.COMMAND_SEPARATOR;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Scanner;
+import java.util.Map;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Arrays;
+
 public class CommandLineInterface {
+    public static final String EXIT = "exit";
+    private final Map<String, Command> commands;
+    private final View view;
+
+    private boolean echoCommands = false;
+
+    public CommandLineInterface(View view) {
+        this.view = view;
+        this.commands = new HashMap<>();
+    }
+
+    public void add(Command command) {
+        this.commands.put(command.name(), command);
+    }
+
+    /* ========= MODO INTERACTIVO ========= */
+    public boolean runInteractive() {
+        this.echoCommands = false;
+        Scanner scanner = new Scanner(System.in).useDelimiter(COMMAND_SEPARATOR);
+        boolean exit;
+        do {
+            exit = this.runCommand(scanner);
+        } while (!exit);
+        return true;
+    }
+
+    /* ========= MODO FICHERO ========= */
+    public boolean runFromFile(String fileName) throws IOException {
+        this.echoCommands = true;
+        try (Scanner scanner = new Scanner(Path.of(fileName)).useDelimiter(COMMAND_SEPARATOR)) {
+            boolean exit;
+            do {
+                exit = this.runCommand(scanner);
+            } while (!exit);
+            return true;
+        }
+    }
+
+    public boolean runCommand(Scanner scanner) {
+        this.view.showCommandPrompt();
+        String command = scanner.next();
+
+        // si venimos de fichero, “eco” del comando
+        if (echoCommands) {
+            this.view.show("> " + command);
+        }
+
+        if (!this.commands.containsKey(command)) {
+            throw new CommandException("Comando '" + command + "' no exists.");
+        }
+
+        String[] params = this.scanParamsIfNeededAssured(scanner, command);
+
+        if (EXIT.equals(command)) {
+            return true;
+        } else {
+            this.commands.get(command).execute(params);
+        }
+        return false;
+    }
+
+    private String[] scanParamsIfNeededAssured(Scanner scanner, String command) {
+        List<String> expectedParams = commands.get(command).params();
+        if (expectedParams.isEmpty()) {
+            return new String[0];
+        }
+        String[] foundParams = scanner.next().split(Command.PARAM_SEPARATOR);
+        if (expectedParams.size() != foundParams.length) {
+            throw new BadRequestException("Parámetros esperados: " + expectedParams +
+                    ", encontrados " + Arrays.toString(foundParams));
+        }
+        return foundParams;
+    }
+
+    public void help() {
+        for (Command command : this.commands.values()) {
+            this.view.show(command.help());
+        }
+    }
 }
