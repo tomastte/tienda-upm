aba68a8aa7481dc97676911a1afe1c8630dd9e97
tomastte <juarezgelardoto@gmail.com>
2025-11-23 01:07:04 +0100

    View class implemented

--- src/main/java/es/upm/etsisi/poo/app2/presentation/view/View.java
+++ src/main/java/es/upm/etsisi/poo/app2/presentation/view/View.java
@@ -1,4 +1,39 @@
 package es.upm.etsisi.poo.app2.presentation.view;
 
+import java.util.List;
+
 public class View {
+
+    public void show(String message) {
+        System.out.println(message);
+    }
+
+    public void showTitle(String tittle) {
+        System.out.println(tittle);
+    }
+
+    public void showCommandPrompt() {
+        System.out.print("UPM->");
+    }
+
+    public void showError(String errorMessage) {
+        System.out.println(errorMessage);
+    }
+
+    public <T> void showEntity(T item) {
+        System.out.println(item);
+    }
+
+    public <T> void showList(String title, List<T> items) {
+        if (items == null || items.isEmpty()) {
+            this.show("No items available yet");
+        } else {
+            this.show(title);
+            int index = 1;
+            for (T item : items) {
+                System.out.println(index + ". " + item);
+                index++;
+            }
+        }
+    }
 }
