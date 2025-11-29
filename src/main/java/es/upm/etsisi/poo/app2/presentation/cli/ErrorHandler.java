09d1bce82d120298241f74f99e78ac23c790f241
tomastte <juarezgelardoto@gmail.com>
2025-11-29 23:55:18 +0100

    fix(layout cli): read file with errors

--- src/main/java/es/upm/etsisi/poo/app2/presentation/cli/ErrorHandler.java
+++ src/main/java/es/upm/etsisi/poo/app2/presentation/cli/ErrorHandler.java
@@ -2,8 +2,6 @@ package es.upm.etsisi.poo.app2.presentation.cli;
 
 import es.upm.etsisi.poo.app2.presentation.view.View;
 
-import java.io.IOException;
-
 public class ErrorHandler {
     public void handlesErrors(CommandLineInterface commandLineInterface,
                               View view, String[] args) {
@@ -11,10 +9,8 @@ public class ErrorHandler {
         view.showInit();
         try {
             if (args.length == 0) {
-                // MODO INTERACTIVO: bucle interno en runCommands()
                 commandLineInterface.runCommands();
             } else {
-                // MODO FICHERO: se ejecuta solo una vez
                 commandLineInterface.runCommandsFromFile(args[0]);
             }
         } catch (Exception e) {
@@ -22,5 +18,4 @@ public class ErrorHandler {
         }
         view.showClose();
     }
-
 }
